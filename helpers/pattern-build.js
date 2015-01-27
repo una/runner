62ea8e8240fe5c3fa6fdc9c1e52f193e588ff431
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-01-27 15:19:46 -0600

    pattern list var being created and filled with names and paths of patterns

--- helpers/pattern-build.js
+++ helpers/pattern-build.js
@@ -12,6 +12,7 @@ var through = require('through2'),
     fs = require('fs-extra'),
     marked = require('marked'),
     PluginError = gutil.PluginError,
+    patternList = [],
     PLUGIN_NAME = 'pattern-build';
 
 //////////////////////////////
@@ -144,7 +145,10 @@ var templateCompile = function (paths, file, options) {
     });
   }
 
-
+  //////////////////////////////
+  // Create a list of patterns for navigation
+  //////////////////////////////
+  patternList.push({'name': paths.inner, 'path': paths.absolute});
 
   return new Buffer(render);
 }
@@ -206,9 +210,11 @@ module.exports = function (options) {
         file.path = paths.folder + '/index.html';
       }
 
+      var util = require('util'); //temp
 
       // Tell the user that stuff's gone down.
       gutil.log('Pattern ' + gutil.colors.magenta(paths.inner) + ' compiled');
+      gutil.log('Pattern List ' + gutil.colors.yellow(util.inspect(patternList))); //temp
     }
 
     //////////////////////////////
